d9b4cb788b89429ea598c951287e94da31f46de6
lexel <alexcodeplace@gmail.com>
2022-12-04 22:55:41 +0100

    initial commit

---
+++ src/main/java/com/mywebsiteproject/MywebsiteprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mywebsiteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebsiteprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywebsiteprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mywebsiteproject/MywebsiteprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywebsiteproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebsiteprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

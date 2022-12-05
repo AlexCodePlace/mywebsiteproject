ec1decd1c4c68b9eb8dc5690b7fafb0e380c10de
lexel <alexcodeplace@gmail.com>
2022-12-05 11:36:24 +0100

    works for fetch('/hello') on localhost:8080 inside and outside .jar

--- src/main/java/com/mywebsiteproject/controller/BasicController.java
+++ src/main/java/com/mywebsiteproject/controller/BasicController.java
@@ -1,10 +1,8 @@
 package com.mywebsiteproject.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class BasicController {
 
@@ -12,4 +10,5 @@ public class BasicController {
     public String sayHello(){
         return "Hello Everyone!";
     }
+
 }

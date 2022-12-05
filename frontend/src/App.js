63722298967ed467ecdb92e0eed1f2cbb9bd7628
lexel <alexcodeplace@gmail.com>
2022-12-05 12:02:56 +0100

    Revert "works for fetch('http://localhost:8080/') on localhost:8080, but not inside .jar"
    This reverts commit cf29dc4b6f7411a28e3032c584a99983ad68f13c.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   }
 
   hello = () => {
-    fetch('http://localhost:8080/')
+    fetch('/hello')
         .then(response => response.text())
         .then(message => {
           this.setState({message: message});
